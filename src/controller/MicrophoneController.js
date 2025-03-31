3defe8473313c12b514ccf7b76219eb5b3192648
diogo <diogo.lima.flooxai@gmail.com>
2025-03-31 09:58:24 -0300

    feat: add methods to capture and record audio from microphone

--- src/controller/MicrophoneController.js
+++ src/controller/MicrophoneController.js
@@ -1,33 +1,98 @@
 import { ClassEvent } from "../util/ClassEvent";
 
-
 export class MicrophoneController extends ClassEvent {
   constructor() {
     super();
+
+    this._available = false;
+
+    this._mimeType = "audio/webm";
 
     navigator.mediaDevices
       .getUserMedia({
         audio: true,
       })
       .then((stream) => {
+        this._available = true;
         this._stream = stream;
 
-        let audio = new Audio();
-
-        audio.srcObject = stream;
-
-        audio.play();
-
-        this.trigger("play", audio);
+        this.trigger("ready", this._stream);
       })
       .catch((err) => {
         console.error(err);
       });
   }
 
+  isAvailable() {
+    return this._available;
+  }
+
   stop() {
     this._stream.getTracks().forEach((track) => {
       track.stop();
     });
+  }
+
+  startRecorder() {
+    if (this.isAvailable()) {
+      this._mediaRecoder = new MediaRecorder(this._stream, {
+        mimeType: this._mimeType,
+      });
+
+      this._recordedChunks = [];
+
+      this._mediaRecoder.addEventListener("dataavailable", (e) => {
+        if (e.data.size > 0) this._recordedChunks.push(e.data);
+      });
+
+      this._mediaRecoder.addEventListener("stop", (e) => {
+        let blob = new Blob(this._recordedChunks, {
+          type: this._mimeType,
+        });
+
+        let filename = `rec${Date.now()}.webm`;
+
+        let file = new File([blob], filename, {
+          type: this._mimeType,
+          lastModified: Date.now(),
+        });
+        console.log("file", file);
+
+        let reader = new FileReader();
+
+        reader.onload = (e) => {
+          console.log("reader file", file);
+
+          let audio = new Audio(reader.result);
+
+          audio.play();
+        };
+
+        reader.readAsDataURL(file);
+      });
+
+      this._mediaRecoder.start();
+      this.startTimer();
+    }
+  }
+
+  stopRecorder() {
+    if (this.isAvailable()) {
+      this._mediaRecoder.stop();
+      this.stop();
+      this.stopTimer();
+    }
+  }
+
+  startTimer() {
+    let start = Date.now();
+
+    this._recordMicrophoneInterval = setInterval(() => {
+      this.trigger('recordTimer', (Date.now() - start));
+    }, 100);
+  }
+
+  stopTimer(){
+    clearInterval(this._recordMicrophoneInterval);
   }
 }
--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -271,23 +271,28 @@ export class WhatsAppController {
     this.el.btnSendMicrophone.on('click', e=>{
       this.el.recordMicrophone.show();
       this.el.btnSendMicrophone.hide();
-      this.startRecordMicrophoneTime();
 
       this._microphoneController = new MicrophoneController();
 
-      this._microphoneController.on('play', musica =>{
-        console.log('recebi o evento play', musica)
+      this._microphoneController.on('ready', musica =>{
+        this._microphoneController.startRecorder()
       })
+
+      this._microphoneController.on('recordtimer', timer => {
+        this.el.recordMicrophoneTimer.innerHTML = Format.toTime(
+          timer
+        );
+      });
 
     });
 
     this.el.btnCancelMicrophone.on('click', e=>{
-      this._microphoneController.stop();
+      this._microphoneController.stopRecorder()
       this.closeRecordMicrophone();
     })
 
     this.el.btnFinishMicrophone.on('click', e=>{
-      this._microphoneController.stop();
+      this._microphoneController.stopRecorder()
       this.closeRecordMicrophone();
     })
 
@@ -356,25 +361,11 @@ export class WhatsAppController {
 
       });
     });
-
-
-
-  }
-
-  startRecordMicrophoneTime(){
-    let start = Date.now();
-
-    this._recordMicrophoneInterval = setInterval(() =>{
-
-      this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start)
-    }, 100)
-
   }
 
   closeRecordMicrophone(){
     this.el.recordMicrophone.hide();
     this.el.btnSendMicrophone.show();
-    clearInterval(this._recordMicrophoneInterval);
   }
 
   closeAllMainPanel(){
