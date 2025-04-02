394ba10a938224601ae1b02a9c01895bb267fbcf
diogo <diogo.lima.flooxai@gmail.com>
2025-04-02 08:26:59 -0300

    feat: add wait, sent and read to update messages

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -164,22 +164,35 @@ export class WhatsAppController {
           this.el.panelMessagesContainer.scrollHeigth -
           this.el.panelMessagesContainer.offsetHeigth;
 
-        let autoScroll = scrollTop >= scrollTopMax;
+        let autoScroll = (scrollTop >= scrollTopMax);
 
         docs.forEach((doc) => {
           let data = doc.data();
           data.id = doc.id;
 
+          let message = new Message();
+
+          message.fromJSON(data);
+
+          let me = data.from === this._user.email;
+
           if (!this.el.panelMessagesContainer.querySelector("#_" + data.id)) {
-            let message = new Message();
 
-            message.fromJSON(data);
-
-            let me = data.from === this._user.email;
+            if(!me) {
+              doc.ref.set({
+                status:'read'
+              },{
+                merge:true
+              })
+            }
 
             let view = message.getViewElement(me);
 
             this.el.panelMessagesContainer.appendChild(view);
+          } else if(me) {
+            let msgEl = this.el.panelMessagesContainer.querySelector("#_" + data.id)
+
+            msgEl.querySelector('.message-status').innerHTML = message.getStatusViewElement().outerHTML;
           }
         });
 
